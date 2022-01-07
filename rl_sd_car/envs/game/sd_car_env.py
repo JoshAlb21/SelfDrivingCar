95c72f2a23f871525f3ff2a1b1d50ad6cb9f4b1c
Joshua <joshua.albiez3@gmail.com>
2022-01-07 15:29:06 +0100

    Add custom env inheritate from gym.Env

---
+++ rl_sd_car/envs/game/sd_car_env.py
@@ -0,0 +1,47 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class SdCarEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self):
+  
+    ...
+  def step(self, action):
+    ...
+  def reset(self):
+    ...
+  def render(self, mode='human'):
+    ...
+  def close(self):
+    ...
+  
+  super(ChopperScape, self).__init__()
+        
+
+        
+        # Define a 2-D observation space
+        self.observation_shape = (600, 800, 3)
+        self.observation_space = spaces.Box(low = np.zeros(self.observation_shape), 
+                                            high = np.ones(self.observation_shape),
+                                            dtype = np.float16)
+    
+        
+        # Define an action space ranging from 0 to 4
+        self.action_space = spaces.Discrete(6,)
+                        
+        # Create a canvas to render the environment images upon 
+        self.canvas = np.ones(self.observation_shape) * 1
+        
+        # Define elements present inside the environment
+        self.elements = []
+        
+        # Maximum fuel chopper can take at once
+        self.max_fuel = 1000
+
+        # Permissible area of helicper to be 
+        self.y_min = int (self.observation_shape[0] * 0.1)
+        self.x_min = 0
+        self.y_max = int (self.observation_shape[0] * 0.9)
+        self.x_max = self.observation_shape[1]
--- rl_sd_car/envs/sd_car_env.py
+++ rl_sd_car/envs/sd_car_env.py
@@ -2,46 +2,61 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+import numpy as np
+from typing import List
+
 class SdCarEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
 
-  def __init__(self):
-  
-    ...
-  def step(self, action):
-    ...
-  def reset(self):
-    ...
-  def render(self, mode='human'):
-    ...
-  def close(self):
-    ...
-  
-  super(ChopperScape, self).__init__()
-        
-
-        
-        # Define a 2-D observation space
-        self.observation_shape = (600, 800, 3)
-        self.observation_space = spaces.Box(low = np.zeros(self.observation_shape), 
-                                            high = np.ones(self.observation_shape),
-                                            dtype = np.float16)
+    metadata = {'render.modes': ['human']}
     
-        
-        # Define an action space ranging from 0 to 4
-        self.action_space = spaces.Discrete(6,)
-                        
-        # Create a canvas to render the environment images upon 
-        self.canvas = np.ones(self.observation_shape) * 1
-        
-        # Define elements present inside the environment
-        self.elements = []
-        
-        # Maximum fuel chopper can take at once
-        self.max_fuel = 1000
+    def __init__(self):
 
-        # Permissible area of helicper to be 
-        self.y_min = int (self.observation_shape[0] * 0.1)
-        self.x_min = 0
-        self.y_max = int (self.observation_shape[0] * 0.9)
-        self.x_max = self.observation_shape[1]
+        super().__init__()
+        
+    
+        # Define an action space
+        self.action_space = spaces.Discrete(5,)
+        '''
+        left
+        right
+        acc
+        brake
+        nothing
+        '''
+
+        # Define observation space
+        '''
+        velocity (only x)
+        angle
+        distance1
+        distance2
+        '''
+        high = np.array([20.0, 360.0, 1.0, 1.0])
+        low = np.array([0.0] * 360)
+        self.observation_space = spaces.Box(low, high, dtype=np.float32)
+
+    def step(self, action):
+
+        # if we took an action, we were in state 1
+        observation = np.array([5.0, 360.0, 1.0, 1.0])
+        if action == 2:
+            reward = 1
+        else:
+            reward = -1
+        # regardless of the action, game is done after a single step
+        done = True
+
+        info = {}
+        
+        return observation, reward, done, info
+
+    def calculate_reward():
+
+    def reset(self):
+        state = 0
+        return state
+
+    def render(self, mode='human'):
+        pass
+    def close(self):
+        pass

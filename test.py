64361d24c202487158b8dfaa738197a9db258ad6
Joshua Albiez <joshua.albiez3@gmail.com>
2022-04-02 01:18:47 +0200

    Implement Framework for testing

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ import gym
 from gym import envs
 import time
 
+from stable_baselines import DQN
+from stable_baselines.deepq.policies import MlpPolicy
+
 env = SdCarEnv()
 action_space_size = env.action_space.n
 state_space_shape = env.observation_space.shape
@@ -18,7 +21,21 @@ print("Sampled action:", action)
 obs, reward, done, info = env.step(action)
 print(obs, reward, done, info)
 
+model = DQN(MlpPolicy, env, verbose=1, prioritized_replay=True)
+# Train the agent
+model.learn(total_timesteps=4000)
+end = time.time()
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+'''
+TEST Function
 while True:
     action = env.action_space.sample()
     obs, reward, done, info = env.step(action)
     print(action)
+'''
